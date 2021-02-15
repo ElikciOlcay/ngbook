a49f2ec7d930bcb4f97a08f8e2188c97ec72d3f2
Olcay Elikci <olcay.elikci@hotmail.com>
2021-02-15 14:44:49 +0100

    add books api to project

--- src/app/shared/book-store.service.ts
+++ src/app/shared/book-store.service.ts
@@ -1,51 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Book } from './book';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookStoreService {
+  private api = 'https://api4.angular-buch.com';
 
-  books: Book[];
+  constructor(private http: HttpClient) {}
 
-  constructor() {
-    this.books = [
-      {
-        isbn: '9783864907791',
-        title: 'Angular',
-        authors: ['Ferdinand Malcher', 'Johannes Hoppe', 'Danny Koppenhagen'],
-        published: new Date(2020, 8, 1),
-        subtitle: 'Grundlagen, fortgeschrittene Themen und Best Practices',
-        rating: 5,
-        thumbnails: [{
-          url: 'https://ng-buch.de/angular-cover.jpg',
-          title: 'Buchcover'
-        }],
-        description: 'Lernen Sie Angular mit diesem Praxisbuch!'
-      },
-      {
-        isbn: '9783864905520',
-        title: 'React',
-        authors: ['Oliver Zeigermann', 'Nils Hartmann'],
-        published: new Date(2019, 11, 12),
-        subtitle: 'Grundlagen, fortgeschrittene Techniken und Praxistipps',
-        rating: 3,
-        thumbnails: [{
-          url: 'https://ng-buch.de/react-cover.jpg',
-          title: 'Buchcover'
-        }],
-        description: 'Das bewährte und umfassende Praxisbuch zu React.'
-      }
-    ];
+  getAll(): Observable<Book[]>  {
+    return this.http.get<any[]>(`${this.api}/books`);
   }
 
-  getAll(): Book[] {
-    console.table(this.books);
-    return this.books;
+  getSingle(isbn: string): Observable<Book> {
+    return this.http.get<Book>(`${this.api}/book/${isbn}`)
   }
 
-  getSingle(isbn: string): Book | undefined {
-    return this.books.find(book => book.isbn === isbn);
+  // response type text, beacause on delete the body is empty. Otherwise it will throw an error.
+  remove(isbn: string): Observable<any> {
+    return this.http.delete(`${this.api}/book/${isbn}`, {responseType: 'text'});
   }
+
 
 }
